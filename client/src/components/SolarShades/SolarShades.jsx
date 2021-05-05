b1924d88e654fb04b43e85b9d3c666f1bbe64c42
kreid333 <kai.j.reid@gmail.com>
2021-05-05 15:50:27 -0400

    removed photo from solar shades page

--- client/src/components/SolarShades/SolarShades.jsx
+++ client/src/components/SolarShades/SolarShades.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import IMG from "../../images/gallery/solarshades/IMG_5186.jpg"
 import IMG1 from "../../images/gallery/solarshades/IMG_5187.jpg"
-import IMG2 from "../../images/gallery/solarshades/IMG_5188.jpg"
 import IMG3 from "../../images/gallery/solarshades/IMG_5189.jpg"
 import IMG4 from "../../images/gallery/solarshades/IMG_5190.jpg"
 import IMG5 from "../../images/gallery/solarshades/IMG_5191.jpg"
@@ -22,30 +21,25 @@ const SolarShades = () => {
       </div>
       <div className="row">
         <div className="col-sm-6 mb-3 imgDiv">
-          <img className="w-100 h-100 galleryImg" src={IMG2} alt="" />
-        </div>
-        <div className="col-sm-6 mb-3 imgDiv">
           <img className="w-100 h-100 galleryImg" src={IMG3} alt="" />
         </div>
-      </div>
-      <div className="row">
         <div className="col-sm-6 mb-3 imgDiv">
           <img className="w-100 h-100 galleryImg" src={IMG4} alt="" />
         </div>
+      </div>
+      <div className="row">
         <div className="col-sm-6 mb-3 imgDiv">
           <img className="w-100 h-100 galleryImg" src={IMG5} alt="" />
         </div>
-      </div>
-      <div className="row">
         <div className="col-sm-6 mb-3 imgDiv">
           <img className="w-100 h-100 galleryImg" src={IMG6} alt="" />
         </div>
+      </div>
+      <div className="row">
         <div className="col-sm-6 mb-3 imgDiv">
           <img className="w-100 h-100 galleryImg" src={IMG7} alt="" />
         </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-12 mb-3 imgDiv">
+        <div className="col-sm-6 mb-3 imgDiv">
           <img className="w-100 h-100 galleryImg" src={IMG8} alt="" />
         </div>
       </div>
