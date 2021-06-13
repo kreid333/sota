37f52b2af27558c4ccb8b213980f4f431777e6b9
kreid333 <kai.j.reid@gmail.com>
2021-06-13 14:50:00 -0400

    testing host change for transporter

--- controller/apiController.js
+++ controller/apiController.js
@@ -27,7 +27,7 @@ router.post("/api/sendMail", (req, res) => {
     try {
       // create reusable transporter object using the default SMTP transport
       let transporter = nodemailer.createTransport({
-        host: "smtp.sotadecor.com",
+        host: "mail.sotadecor.com",
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
